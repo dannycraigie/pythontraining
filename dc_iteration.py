189748db06b8c948cec6c27a06b478ff55ca1fd0
Danny Craigie <dannycraigie@outlook.com>
2022-01-25 17:19:45 +0000

    Tuesday functions, iterations and modules

---
+++ dc_iteration.py
@@ -0,0 +1,77 @@
+# counts down from 5 using while statement
+countdown = 5
+while countdown > -1:
+    print(countdown)
+    countdown = countdown -1
+
+# While is used for ongoing iterations while a condition is not met
+# For is a specific number of iterations as in a list and must be indexed. So not dictionaries or sets
+
+for i in ["cat", "dog", "Lemur", "snake"]:
+    print(i)
+
+# prints 0 - 5 6 characters including 0 using RANGE
+for i in range(6):
+    print(i)
+
+# countsdown from 20 to 5 in -2 blocks
+for i in range(20, 5, -2):
+    print(i)
+
+# prints the number of pies you have eaten from 0 - 5
+#for i in range(5):
+#    print(f"I have eaten {i} pies")
+
+
+# i searches the list for the "statement"
+#cool_animals = ["Winnie the Moo", "Moolan", "Milkshake", "Kevin Bacon"]
+#for i in cool_animals:
+#       if i == "Moolan":
+#        continue # break would end the loop, Continue continues the process
+#       print(i) 
+
+
+# count = 0
+# name = str(input("What is your name?"))
+
+# while count < 1:
+#     print(name, "is awesome!")
+#    count += 1
+#    while count < 5:
+#    name = str(input("What is your name?"))
+#    print(name, "Is awesome")
+
+    
+# counts from 5 to 10
+#for i in range(5, 11):
+#    print(i)
+
+# counts from 10-20 in 2s
+#for i in range(10, 21, 2):
+#    print(i)
+
+#for i in range (1501, 2701):
+#    if i %7 == 0 and i %5 == 0:
+#        print(i)
+
+# number = int(input("Guess a number: ?"))
+# for i in number:
+#    if  i >9:
+#        continue
+#         
+#        print("Correct you guessed between 1 and 9")
+
+
+# prints range from 0-6 missing out 3 and 6
+#for i in range (0, 7):
+#    if (i ==3 or i ==6):
+#        continue
+#    print(i)
+
+
+
+
+
+
+
+
---
+++ dc_modules.py
@@ -0,0 +1,36 @@
+#import dc_functions
+# imports whole page
+
+# imports the specific file from the page
+#from dc_functions import per_hundred
+
+# renames file with and alias in case of conflict
+#from dc_functions import per_hundred as isapercentage
+
+# imported the math module with square root func
+#import math
+#number = float(input("Enter a number: "))
+#answer = math.sqrt(number)
+#print(answer)
+
+#import math
+#import random
+#def random_pi():
+#    return math.ceil(random.randint(1, 10) * math.pi)
+#
+#for _ in range(5):
+#    print(random_pi())
+
+#from math import pi, ceil
+#from random import randint
+#
+#def random_pi():
+#    return ceil(randint(1, 10) * pi)
+#
+#for _ in range(5):
+#    print(random_pi())
+
+#import requests
+#
+#print(requests.get("https://google.com"))
+
